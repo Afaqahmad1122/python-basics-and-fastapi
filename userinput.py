d096132170a7b178009ea68f3ecf059ace2942a0
Afaq Ahmad <afaqahmadnaeem@gmail.com>
2026-04-23 12:13:44 +0500

    list practice

--- userinput.py
+++ userinput.py
@@ -1,2 +1,10 @@
-first_name = input("Enter your first name a")
-print(first_name)
+person_names = ["afaq", "ali", "ahmad"]
+
+# print(len(person_names))
+# print(person_names[0:2])
+
+# append element in array
+person_names.append("zaheer bacha")
+print(person_names)
+person_names.insert(2, "Suleman")
+print(person_names)
