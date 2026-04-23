2c974071e034849b26797e79fbc39542d3c2f8af
Afaq Ahmad <afaqahmadnaeem@gmail.com>
2026-04-23 11:50:14 +0500

    string concate

--- pythonBasics.py
+++ pythonBasics.py
@@ -1,7 +1,14 @@
-money = 50
-item = 15
-tax = .03
+# money = 50
+# item = 15
+# tax = .03
 
-money_left = money - item - (item * tax)
+# money_left = money - item - (item * tax)
 
-print(money_left)
+# print(money_left)
+
+# concate
+# name = "Afaq"
+# print("hi " + name)
+
+name = "tanveer"
+print(f"Hi {name}")
