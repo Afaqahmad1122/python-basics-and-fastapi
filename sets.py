34c1b4e85229c6bfbeccf73658b12e7cda21a16c
Afaq Ahmad <afaqahmadnaeem@gmail.com>
2026-04-23 14:03:48 +0500

    sets

---
+++ sets.py
@@ -0,0 +1,14 @@
+# def my_coutry(name = "Pakistan"):
+#     print("My country is " + name)
+    
+    
+# my_coutry("Norway")
+
+# highest and lowest number
+
+def number_high_low(highest_number, lowest_number):
+    print(f"This is highest number {highest_number}"
+    )
+    print(f"This is lowest number {lowest_number}")
+
+number_high_low(12,3)
